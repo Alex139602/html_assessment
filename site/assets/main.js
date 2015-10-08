622186a882ad56fced86e7e68da9e287c48f461e
Alex139602 <Alexrw17@gmail.com>
2015-10-08 16:13:44 +0100

    Updated navbar and canvas, and answered questions

--- site/assets/main.js
+++ site/assets/main.js
@@ -1,8 +1,39 @@
 $(document).ready(function(){
-	var thisFile = window.location.href.split("/");
-	var fileName = thisFile[thisFile.length -1];
-	var listOfURLs = ['home.html', 'about.html', 'contact.html','order.html','signup.html'];
-	for (var i = 0; i < listOfURLs.length; i++) {
-		console.log(listOfURLs[i].toString() == fileName.toString());
-	};
+	function colorNavigation(){
+		var thisFile = window.location.href.split("/");
+		var fileName = thisFile[thisFile.length -1];
+		var links = $('#nav').children().children();
+		console.log(links);
+		for (var i = 0; i < links.length; i++) {
+			if (links[i].id == fileName) {
+				links[i].style.color="#ff0000";
+			};
+		};
+	}
+
+	function logThings(){
+		console.log($('#gear-10'));
+		console.log($('#gear-6'));
+		console.log($('#gear-8'));
+	}
+
+	function drawImages(){
+		var imageIds = ['gear-10', 'gear-6', 'gear-8'];
+		var canvasIds = [ 'canvas-' + imageIds[0], 'canvas-' + imageIds[1], 'canvas-' + imageIds[2] ];
+		var gear10 = document.getElementById(imageIds[0]);
+		var gear6 = document.getElementById(imageIds[1]);
+		var gear8 = document.getElementById(imageIds[2]);
+		var gearElemnentIdArrays = [document.getElementById(imageIds[0]), document.getElementById(imageIds[1]), document.getElementById(imageIds[1])];
+		for (var i = 0; i < gearElemnentIdArrays.length; i++) {
+			console.log(gearElemnentIdArrays[i]);
+		};
+	}
+
+	function runAll(){
+		colorNavigation();
+		logThings();
+		drawImages();
+	}
+
+	runAll();
 });
